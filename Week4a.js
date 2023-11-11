44729bdfcb281cb9eb88d8e87e36e0b7bf8ac728
Jordan Charles <jccharles@email.neit.edu>
2023-11-11 18:22:46 -0500

    .

--- Week4a.js
+++ Week4a.js
@@ -4,5 +4,5 @@ if (wideScope ==7) {
     console.log("localScope inside if-block = " + localScope)
     console.log("wideScope inside if-block = " + wideScope);
 }
-//console.log("localScope outside if-block = " + localScope);
+console.log("localScope outside if-block = " + localScope);
 console.log("wideScope outside if-block = " + wideScope);
