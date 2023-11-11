8ab5b43c5d7a793e95e02675e2c967f7712b9081
Jordan Charles <jccharles@email.neit.edu>
2023-11-11 17:39:21 -0500

    .

---
+++ Quest3.js
@@ -0,0 +1,17 @@
+console.log("Red Team");
+console.log("        ");
+
+var w = 0;
+var c = 0;
+var o = 0;
+var e = 0;
+
+w = prompt("Enter the Weapon Control value: ");
+c = prompt("Enter the Captures value: ");
+o = prompt("Enter the Ownages value: ");
+e = prompt("Enter the Effieciency value: ");
+
+console.log("Weapon Control: " + w + "%");
+console.log("Captures: " + c + "%");
+console.log("Ownages: " + o + "%");
+console.log("Efficiency: " + e + "%");
