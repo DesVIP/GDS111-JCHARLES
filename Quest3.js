ca41e2a4a344ead1288fae871d28a826c50281ae
Jordan Charles <jccharles@email.neit.edu>
2023-11-15 13:59:33 -0500

    .

--- Quest3.js
+++ Quest3.js
@@ -1,12 +1,25 @@
-var coupons = 0;
-remainingCoupons = coupons % 4;
+// var coupons = 0;
+var coupons = prompt("Candy calculator. Enter number of coupons to redeem: ");
 
 
-coupons = prompt("Candy calculator. Enter number of coupons to redeem: ");
+var couponsaftercandy = coupons % 10;
+
+var temp = coupons - couponsaftercandy
+
+var cba = temp/10;
+
+var couponsaftergum = couponsaftercandy % 3;
+
+temp = couponsaftercandy - couponsaftergum;
+
+var gba = temp/3;
+
+
+
 console.log("Candy calculator. Enter number of coupons to redeem:");
 console.log(coupons);
 
 
 
  
-console.log("Our " + coupons + " coupons can be redeemed for " + Math.floor(1.5) + " candy bars and " + Math.floor(1.5) + " gumballs with " + remainingCoupons + " coupons leftover.")
+console.log("Our " + coupons + " coupons can be redeemed for " + cba +  " candy bars and " + gba + " gumballs with " + couponsaftergum + " coupons leftover.")
