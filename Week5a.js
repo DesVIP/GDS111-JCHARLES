205144da7b803884c0919820ebcc9d4f90298c05
Jordan Charles <jccharles@email.neit.edu>
2023-11-15 19:06:11 -0500

    .

---
+++ Week5a.js
@@ -0,0 +1,10 @@
+//let num1 = Math.random();
+//console.log(num1);
+
+let x = Math.floor(Math.random() * 11) + 1;
+console.log("The numer is: " + x);
+if (x == 7 || x == 11) {
+  console.log("Congratulations, you win!");
+} else {
+  console.log("Sorry, you lost.");
+}
