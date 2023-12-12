4406e0b100763bba03820b06872e3ee0f6f86030
DesVIP <jccharles@email.neit.edu>
2023-12-12 11:12:35 -0500

    .

--- Final.js
+++ Final.js
@@ -1,26 +1,36 @@
+
 let numberGenerator = Math.floor(Math.random() * 100) + 1;
 var userInput = 0;
 let trialCount = 1;
+var secretNum = 0;
 
+userInput = prompt("Guess a number")
+console.log(userInput)
 
+secretNum = numberGenerator
 
-function guessChecker() {
-    
-    if (userInput == numberGenerator) {
+    if (userInput == secretNum) {
         console.log("Hooray! You guessed it in " + trialCount + " attempts")
     } else if (trialCount === 5) {
         console.log("Sorry, YOU LOSE!")
     }
-    if(userInput < numberGenerator) {
+    if(userInput > secretNum) {
         console.log("Your guess is lower - try again")
-    } else if (userInput > numberGenerator) {
+    
+    } else if (userInput < secretNum) {
         console.log("Your guess is higher - try again")
+       
     }
-
+ 
     trialCount++;
+    
+   
+    
 
-    userInput = prompt("Guess a number")
-}
+    
+
+    
+
 
 
 
